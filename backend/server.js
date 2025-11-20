decf6b640ac6c4902d9e3d31bb7b87284ff423b2
omkar <omkarmane1714@gmail.com>
2025-11-20 17:43:40 +0530

    Updated server.js & sequelize.js for production ready

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,18 @@ import userRoutes from "./routes/users.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// ----------- cors ------------------
+
+app.use(
+  cors({
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // ----------- ROUTES -------------
