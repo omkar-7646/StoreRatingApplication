f57714e99f35653bf47146023bfefe9103ff6628
omkar <omkarmane1714@gmail.com>
2025-11-21 14:51:45 +0530

    modified cors & added published frontend url

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL || "http://localhost:3000",
+      "https://storeratingapplication.netlify.app/" || "http://localhost:3000",
     ],
     credentials: true,
   })
@@ -50,3 +50,6 @@ dbInit().then(() => {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
+
+
+//  https://storeratingapplication.netlify.app/
